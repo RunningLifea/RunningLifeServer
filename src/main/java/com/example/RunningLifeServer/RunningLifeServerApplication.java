1ae9e8c8cbab28a2578510e227bfe9ab94cd94f9
yuseogi0218 <yuseogi0218@gmail.com>
2021-12-01 22:44:30 +0900

    init

---
+++ src/main/java/com/example/RunningLifeServer/RunningLifeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.RunningLifeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningLifeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningLifeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RunningLifeServer/RunningLifeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RunningLifeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningLifeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
